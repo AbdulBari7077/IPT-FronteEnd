8504826e347a87e75ba3f18fb73bd2e1736952fc
rasanmemon <rasanmemon@gmail.com>
2022-11-05 16:41:00 +0500

    rate added

--- src/components/rating/index.js
+++ src/components/rating/index.js
@@ -1,18 +1,30 @@
 import Rating from 'material-ui-rating';
 import * as React from 'react';
+import { MuiThemeProvider,createTheme } from '@material-ui/core';
 
 export default function Rate(){
-    
+   
     const [value, setValue] = React.useState(2);
-
+    const myTheme = createTheme({
+        overrides:{
+            MuiSvgIcon:{
+                root:{
+                    color:'red'
+                }
+            }
+        }
+    })
 
     return(
+        <MuiThemeProvider theme={myTheme}>
+
         <Rating
   value={value}
   max={5}
   onChange={(value) => setValue(value) +console.log(value)}
   
-
+  
 />
+  </MuiThemeProvider>
     );
 }
