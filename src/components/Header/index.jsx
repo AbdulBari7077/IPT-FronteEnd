77a539ee2cc51773de3bb8b5aaeaf693f489f88e
AbdulBari7077 <a.bari.ab64@gmail.com>
2022-11-12 00:27:14 -0800

    User header hover added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -11,9 +11,15 @@ function Header({ black }) {
           <img alt="Netflix" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1280px-Netflix_2015_logo.svg.png" />
         </a>
       </div>
-      <a className="header--user" href="/">
-        <img alt="" src="https://learning.royalbcmuseum.bc.ca/wp-content/uploads/2014/07/netflix-face.jpg" />
-      </a>
+      <div class="dropdown">
+        <div class="dropbtn">
+          <img alt="" src="https://learning.royalbcmuseum.bc.ca/wp-content/uploads/2014/07/netflix-face.jpg" />
+        </div>
+        <div class="dropdown-content">
+          <a href="#">Manage Profile</a>
+          <a href="#">Sign out of netflix</a>
+        </div>
+      </div>
     </header>
 
   );
