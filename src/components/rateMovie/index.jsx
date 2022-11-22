4eb5fb5c95c63782bb0b241a45e8c2fd5eaabd98
AbdulBari7077 <a.bari.ab64@gmail.com>
2022-11-22 19:23:48 +0500

    rate movie merged

--- src/components/rateMovie/index.jsx
+++ src/components/rateMovie/index.jsx
@@ -8,45 +8,47 @@ import "./rateMovie.css"
 const RateDialog = (props) => {
   const myTheme = createTheme({
     overrides: {
-      
+
     }
   },
   );
-  const { open, setOpen, handleCloseRateDialog, setRateValue, rateValue} = props;
+  const { open, setOpen, handleCloseRateDialog, setRateValue, rateValue } = props;
   const [value, setValue] = React.useState(rateValue);
-  
 
- 
+
+
   const handleClose = () => {
-      setOpen(false);
-      handleCloseRateDialog();
-    };
-    
+    setOpen(false);
+    handleCloseRateDialog();
+  };
 
-    
-    return (
-      <Dialog  onClose={handleClose} open={open}>
-      <DialogContent  className='Dialograting' style={{backgroundColor:''}}>
+
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogContent className='Dialograting' style={{ backgroundColor: '' }}>
         <DialogContentText>
           <ThemeProvider theme={myTheme}>
-          <Rating
-          style={{color:'red',stroke:'red'}}
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
+            <Rating
+              // defaultValue={1.5} 
+              // precision={0.5}
+              style={{ color: 'red', stroke: 'red' }}
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+            />
           </ThemeProvider>
         </DialogContentText>
       </DialogContent>
-      <DialogActions style={{backgroundColor:'black'}}>
-        <Button onClick={()=> {setRateValue(value)+ setOpen(false)}} style={{color:'red'}}> Submit</Button>
-      <Button onClick={handleClose} autoFocus style={{color:'red'}}> 
-            Close
-          </Button>
+      <DialogActions style={{ backgroundColor: 'black' }}>
+        <Button onClick={() => { setRateValue(value) + setOpen(false) }} style={{ color: 'red' }}> Submit</Button>
+        <Button onClick={handleClose} autoFocus style={{ color: 'red' }}>
+          Close
+        </Button>
       </DialogActions>
     </Dialog>
-                     
+
   );
 };
 export default RateDialog;
