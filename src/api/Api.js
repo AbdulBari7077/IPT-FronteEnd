9bc9d2ae67f6f012181bf0580fd265455e2529ca
AbdulBari7077 <a.bari.ab64@gmail.com>
2022-12-07 11:26:47 +0500

    all done

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = "https://localhost:44318"
+const BASE_URL = "http://128.199.68.100"
 export const LoginApi = async (userEmail, password) => {
     try {
         const response=await  axios.post(`${BASE_URL}/user/login`, {
